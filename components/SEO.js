54c13155fad2be1d60fa89f8db74f335e245ceaf
Darkee <debojitsantra48@gmail.com>
2023-05-20 17:52:30 +0530

    update components/SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -3,9 +3,11 @@ import Head from 'next/head';
 export default function SEO({ title, description }) {
   return (
     <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
+      <title>Urban Rahsoi</title>
+      <meta name="description" content="UrbanRahsoi, a modern Indian food blog. Our passion for cooking, combined with our love for Indian cuisine, has inspired us to make new recipes." />
+      <meta property="og:title" content="Urban Rahsoi" />
+      <meta property="og:description" content="UrbanRahsoi, a modern Indian food blog." />
+      
     </Head>
   );
 }
